a0be5b3557c21a717113dd0b7c28b4ae32163989
Sarah Burgess <sarahburgess287@gmail.com>
2019-04-18 09:48:10 -0700

    Added test for game component, updated top-nav tests to include mock calls

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import { TopNav } from './top-nav';
+import {RESTART_GAME, GENERATE_AURAL_UPDATE} from '../actions';
 
 describe('<TopNav />', () => {
   it('Renders without crashing', () => {
@@ -17,16 +18,20 @@ describe('<TopNav />', () => {
       preventDefault() {}
     });
     expect(dispatch).toHaveBeenCalled();
+    const action = dispatch.mock.calls[0][0];
+    expect(action.type).toEqual(RESTART_GAME);
+    expect(action.correctAnswer).toBeGreaterThanOrEqual(0);
+    expect(action.correctAnswer).toBeLessThanOrEqual(100);
   });
 
   it('Should call onGenerateAuralUpdate when state-of-game link is clicked', () => {
     const callback = jest.fn();
-
     const wrapper = shallow(<TopNav dispatch={callback} />);
     const link = wrapper.find('.status-link');
     link.simulate('click', {
       preventDefault() {}
     });
     expect(callback).toHaveBeenCalled();
+    expect(callback.mock.calls[0][0].type).toEqual(GENERATE_AURAL_UPDATE);
   });
 });
