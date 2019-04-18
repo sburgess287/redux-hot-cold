efcfd4fb2220d8c0965abee25d66b56d04088f3f
Sarah Burgess <sarahburgess287@gmail.com>
2019-04-17 18:07:54 -0700

    Adding GuessForm tests for react redux

---
+++ src/components/guess-form.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import { GuessForm } from './guess-form';
+
+// failing, not sure how to fix
+describe('<GuessForm />', () => {
+  it('Renders without crashing', () => {
+    shallow(<GuessForm />);
+  });
+
+  it('Should fire the onMakeGuess callback when the form is submitted', () => {
+    const dispatch = jest.fn();
+    const wrapper = mount(<GuessForm onMakeGuess={dispatch} />);
+    const value = 10;
+    wrapper.find('input[type="number"]').instance().value = value;
+    wrapper.simulate('submit');
+    expect(dispatch).toHaveBeenCalledWith(value.toString());
+  });
+
+  it('Should reset the input when the form is submitted', () => {
+    const wrapper = mount(<GuessForm />);
+    const input = wrapper.find('input[type="number"]');
+    input.instance().value = 10;
+    wrapper.simulate('submit');
+    expect(input.instance().value).toEqual('');
+  });
+})
